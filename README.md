3b5a240b0b60594df0f7ee72c2bce8eee8ba696d
Azathothas <AjamX101@gmail.com>
2024-11-25 14:55:14 +0545

    init

---
+++ README.md
@@ -0,0 +1,143 @@
+> Note: You will need [modern fonts](https://github.com/ryanoasis/nerd-fonts) to be able to view `Flags`
+> - Specify `--ipv4 | --ipv6` or similar for `ipv4|ipv6` only data
+---
+- #### Simple (`text`) : [/](https://ip.pkgforge.dev/) `-->` [https://ip.pkgforge.dev/](https://ip.pkgforge.dev/)
+> Returns plain `IPv4 | IPv6`
+```bash
+curl -qfsSL "https://ip.pkgforge.dev"
+```
+---
+- #### Detailed (`csv`) : [/csv](https://ip.pkgforge.dev/csv) `-->` [https://ip.pkgforge.dev/csv](https://ip.pkgforge.dev/csv)
+> Returns csv `IPv4 | IPv6`, `GeoInfo` & `User-Agent`
+```bash
+!#Using: curl + column
+curl -qfsSL "https://ip.pkgforge.dev/csv" | column -ts ','
+```
+> <details><summary><code>Example Response</code></summary>
+>
+>```
+> ip           city       country  flag  region     latitude   longitude  org         timezone           user-agent   readme
+> 18.227.3.14  São Paulo  BR       🇧🇷    São Paulo  -23.53350  -46.63590  Amazon.com  America/Sao_Paulo  curl/7.88.1  https://github.com/pkgforge-dev/ip.pkgforge.dev
+>```
+> </details>
+---
+- #### Detailed (`html`) : [/html](https://ip.pkgforge.dev/html) `-->` [https://ip.pkgforge.dev/html](https://ip.pkgforge.dev/html)
+> Returns HTML `IPv4 | IPv6`, `GeoInfo` & `User-Agent`
+> <details><summary><code>Example Response</code></summary>
+>
+> ![image](https://github.com/pkgforge-dev/ip.pkgforge.dev/assets/58171889/39b9dd85-a2f0-4eb1-90c7-961ba6bc98f2)
+> 
+> </details>
+---
+- #### Detailed (`json`) : [/json](https://ip.pkgforge.dev/json) `-->` [https://ip.pkgforge.dev/json](https://ip.pkgforge.dev/json)
+> Returns json `IPv4 | IPv6`, `GeoInfo` & `User-Agent`
+```bash
+!#Using: curl + https://github.com/jqlang/jq
+curl -qfsSL "https://ip.pkgforge.dev/json" | jq '.'
+```
+> <details><summary><code>Example Response</code></summary>
+>
+> ```json
+> {
+> "ip": "18.227.3.14",
+> "city": "São Paulo",
+> "country": "BR",
+> "flag": "🇧🇷",
+> "region": "São Paulo",
+> "latitude": "-23.53350",
+> "longitude": "-46.63590",
+> "org": "Amazon.com",
+> "timezone": "America/Sao_Paulo",
+> "user-agent": "curl/7.88.1",
+> "readme": "https://github.com/pkgforge-dev/ip.pkgforge.dev"
+> }
+> ```
+> </details>
+---
+- #### Detailed (`text`) : [/text](https://ip.pkgforge.dev/text) `-->` [https://ip.pkgforge.dev/text](https://ip.pkgforge.dev/text)
+> Returns text `IPv4 | IPv6`, `GeoInfo` & `User-Agent`
+```bash
+curl -qfsSL "https://ip.pkgforge.dev/text"
+```
+> <details><summary><code>Example Response</code></summary>
+>
+> ```bash
+> ip=18.227.3.14
+> city=São Paulo
+> country=BR
+> flag=🇧🇷
+> region=São Paulo
+> latitude=-23.53350
+> longitude=-46.63590
+> org=Amazon.com
+> timezone=America/Sao_Paulo
+> user-agent=curl/7.88.1
+> readme=https://github.com/pkgforge-dev/ip.pkgforge.dev
+> ```
+> </details>
+---
+- #### Detailed (`xml`) : [/xml](https://ip.pkgforge.dev/xml) `-->` [https://ip.pkgforge.dev/xml](https://ip.pkgforge.dev/xml)
+> Returns xml `IPv4 | IPv6`, `GeoInfo` & `User-Agent`
+```bash
+!#Using: curl + https://github.com/sibprogrammer/xq
+curl -qfsSL "https://ip.pkgforge.dev/xml" | xq
+```
+> <details><summary><code>Example Response</code></summary>
+>
+> ```xml
+> <?xml version="1.0" encoding="UTF-8"?>
+> <data>
+>   <ip>18.227.3.14</ip>
+>   <city>São Paulo</city>
+>   <country>BR</country>
+>   <flag>🇧🇷</flag>
+>   <region>São Paulo</region>
+>   <latitude>-23.53350</latitude>
+>   <longitude>-46.63590</longitude>
+>   <org>Amazon.com</org>
+>   <timezone>America/Sao_Paulo</timezone>
+>   <user-agent>curl/7.88.1</user-agent>
+>   <readme>https://github.com/pkgforge-dev/ip.pkgforge.dev</readme>
+> </data>
+> ```
+> </details>
+---
+- #### Detailed (`yaml`) : [/yaml](https://ip.pkgforge.dev/yaml) `-->` [https://ip.pkgforge.dev/yaml](https://ip.pkgforge.dev/yaml)
+> Returns yaml `IPv4 | IPv6`, `GeoInfo` & `User-Agent`
+```bash
+!#Using: curl + https://github.com/mikefarah/yq
+curl -qfsSL "https://ip.pkgforge.dev/yaml" | yq '.'
+```
+> <details><summary><code>Example Response</code></summary>
+>
+> ```yaml
+> ip: "18.227.3.14"
+> city: "São Paulo"
+> country: "BR"
+> flag: "🇧🇷"
+> region: "São Paulo"
+> latitude: "-23.53350"
+> longitude: "-46.63590"
+> org: "Amazon.com"
+> timezone: "America/Sao_Paulo"
+> user-agent: "curl/7.88.1"
+> readme: "https://github.com/pkgforge-dev/ip.pkgforge.dev"
+> ```
+> </details> 
+---
+#### Deploy (Note to Self)
+```bash
+!# Install Wrangler: https://developers.cloudflare.com/workers/wrangler/install-and-update/
+npm install "wrangler@latest" -g
+
+!# Clone Source
+git clone --filter="blob:none" "https://github.com/pkgforge-dev/ip.pkgforge.dev"
+cd "./ip.pkgforge.dev"
+code "./ip.pkgforge.dev"
+
+!# Deploy
+npm install ; wrangler login
+wrangler deploy
+
+!# Dashboard >> Workers & Pages >> ip-api >> Settings >> Custom Domains >> Add: ip.pkgforge.dev
+```
---
+++ index.js
@@ -0,0 +1,245 @@
+// plagiarized from: https://github.com/ccbikai/ip-api
+
+// func to get flags
+var EMOJI_FLAG_UNICODE_STARTING_POSITION = 127397;
+function getFlag(countryCode) {
+  const regex = new RegExp("^[A-Z]{2}$").test(countryCode);
+  if (!countryCode || !regex)
+    return void 0;
+  return String.fromCodePoint(
+    ...countryCode.split("").map((char) => EMOJI_FLAG_UNICODE_STARTING_POSITION + char.charCodeAt(0))
+  );
+}
+
+// Function to convert filteredJson to CSV
+function jsonToCsv(json) {
+    let csv = '';
+    // Extract keys from the first object in the array
+    const keys = Object.keys(json[0]);
+    // Write header row
+    csv += keys.join(',') + '\n';
+    // Write data rows
+    json.forEach(item => {
+        const values = keys.map(key => item[key]);
+        csv += values.join(',') + '\n';
+    });
+    return csv;
+}
+
+// Function to convert filteredJson to HTML
+function jsonToHtml(json) {
+  let html = '<!DOCTYPE html>\n<html>\n<head>\n<title>IP Details</title>\n<style>';
+  // Dark mode styles with increased contrast
+  html += `
+    body {
+      background-color: #0d0e0e;
+      color: #f2f0ec;
+      font-family: Arial, sans-serif;
+      line-height: 1.6;
+    }
+    table {
+      border-collapse: collapse;
+      width: 100%;
+      margin-top: 20px;
+    }
+    th, td {
+      border: 1px solid #3b4043;
+      padding: 8px;
+      text-align: left;
+      background-color: #1b1d1e; /* Same color for all table entries */
+    }
+    td {
+      font-weight: bold; /* Bolder entries */
+      color: #c4c1ba; /* Slightly darker shade for values */
+    }
+    th {
+      background-color: #1b1d1e;
+      color: #f2f0ec;
+      font-weight: bold;
+    }
+    tr:nth-child(even) {
+      background-color: #0d0e0e;
+    }
+    a {
+      color: #4ca5ff; /* Blue for links */
+    }
+  `;
+  html += '</style>\n</head>\n<body>\n<table>\n';
+  for (const [key, value] of Object.entries(json)) {
+    html += `<tr><td>${key}</td><td>${isURL(value) ? `<a href="${value}">${value}</a>` : value}</td></tr>\n`;
+  }
+  html += '</table>\n</body>\n</html>';
+  return html;
+}
+// Function to check if a string is a URL or IPv6 address
+function isURL(str) {
+  const urlPattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3})|'+ // OR ipv4 address
+    '([0-9a-fA-F]{1,4}:){1,7}:?([0-9a-fA-F]{1,4})?)'+ // OR ipv6 address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return urlPattern.test(str);
+}
+
+// Function to convert filteredJson to XML
+function jsonToXml(json) {
+    let xml = '<?xml version="1.0" encoding="UTF-8"?>\n<data>\n';
+    for (const [key, value] of Object.entries(json)) {
+      xml += `<${key}>${value}</${key}>\n`;
+    }
+    xml += '</data>';
+    return xml;
+}
+
+//func to convert json to yaml
+function jsonToYaml(json) {
+  const yamlLines = [];
+  function parseObject(obj, indentLevel = 0) {
+    const indent = ' '.repeat(indentLevel * 2);
+    for (const [key, value] of Object.entries(obj)) {
+      if (value === undefined) {
+        continue; // Skip undefined values
+      }
+      if (typeof value === 'object' && value !== null) {
+        yamlLines.push(`${indent}${key}:`);
+        parseObject(value, indentLevel + 1);
+      } else if (typeof value === 'string') {
+        yamlLines.push(`${indent}${key}: "${value}"`);
+      } else {
+        yamlLines.push(`${indent}${key}: ${value}`);
+      }
+    }
+  }
+  parseObject(json);
+  return yamlLines.join('\n');
+}
+
+// Allow Access from any origin
+var CORS_HEADERS = {
+  "Access-Control-Allow-Origin": "*"
+};
+
+// Main Func
+var src_default = {
+  fetch(request) {
+    const ip = request.headers.get("x-real-ip");
+    const userAgent = request.headers.get("user-agent");
+    //Other Headers can also be extracted from response
+    //cf-connecting-ip --> IP of Request Initiator
+    //cf-ipcountry --> Country Code of Request Initiator
+    //x-real-ip --> Real IP of Request Initiator
+    //accept-language --> Locale of Request Initiator (example: en-US,en;q=0.5)
+    const { pathname } = new URL(request.url);
+    console.log(ip, pathname);
+
+   //Generate JSON 
+    const json = {
+      "ip": ip,
+      "city": request.cf.city,
+      "country": request.cf.country,
+      "flag": getFlag(request.cf.country),
+      "region": request.cf.region,
+      "latitude": request.cf.latitude,
+      "longitude": request.cf.longitude,
+      "org": request.cf.asOrganization,
+      "timezone": request.cf.timezone,
+      "user-agent": userAgent,
+      "readme": "https://github.com/pkgforge-dev/ip.pkgforge.dev"
+    };
+
+   // Filter out undefined values
+    const filteredJson = Object.fromEntries(
+      Object.entries(json).filter(([key, value]) => value !== undefined)
+    );
+
+   //Print csv if request is to ip.pkgforge.dev/csv
+   if (pathname === "/csv") {
+        const csvStr = jsonToCsv([filteredJson]);
+        console.log(csvStr);
+        return new Response(csvStr, {
+            headers: {
+                ...CORS_HEADERS,
+                "Content-Type": "text/csv",
+        }
+    });
+
+   //Print html if request is to ip.pkgforge.dev/html
+    } else if (pathname === "/html") {
+        let htmlStr = jsonToHtml(filteredJson);
+        console.log(htmlStr);
+        const endpointsRow = `<tr><td>endpoints</td><td><a href="https://ip.pkgforge.dev/csv">/csv</a> <a href="https://ip.pkgforge.dev/ip">/ip</a> <a href="https://ip.pkgforge.dev/json">/json</a> <a href="https://ip.pkgforge.dev/text">/text</a> <a href="https://ip.pkgforge.dev/xml">/xml</a> <a href="https://ip.pkgforge.dev/yaml">/yaml</a></td></tr>`;
+        htmlStr = htmlStr.replace('</table>\n</body>\n</html>', `${endpointsRow}</table>\n</body>\n</html>`);
+        return new Response(htmlStr, {
+            headers: {
+                ...CORS_HEADERS,
+                "Content-Type": "text/html;charset=utf-8",
+            }
+        });
+
+   //Print json if request is to ip.pkgforge.dev/json
+    } else if (pathname === "/json") {
+        console.log(filteredJson);
+        const jsonResponse = JSON.stringify(filteredJson);
+        return new Response(jsonResponse, {
+            headers: {
+                ...CORS_HEADERS,
+                "Content-Type": "application/json",
+            }
+        });
+
+   //Print text if request is to ip.pkgforge.dev/text        
+    } else if (pathname === "/text") {
+        const textEntries = Object.entries(filteredJson)
+        .map(([key, value]) => `${key}=${value}`);
+        const textStr = textEntries.join('\n');
+        console.log(textStr);
+        return new Response(textStr, {
+          headers: {
+            ...CORS_HEADERS,
+            "Content-Type": "text/plain;charset=utf-8",
+          }
+        });
+
+   //Print xml if request is to ip.pkgforge.dev/xml
+    } else if (pathname === "/xml") { // Add XML handling
+        const xmlStr = jsonToXml(filteredJson);
+        console.log(xmlStr);
+        return new Response(xmlStr, {
+          headers: {
+            ...CORS_HEADERS,
+            "Content-Type": "application/xml",
+          }
+        });
+
+   //Print yaml if request is to ip.pkgforge.dev/yaml  
+    } else if (pathname === "/yaml") {
+      const yamlStr = jsonToYaml({
+        ip,
+        ...json
+      });
+      console.log(yamlStr);
+      return new Response(yamlStr, {
+        headers: {
+          ...CORS_HEADERS,
+          "Content-Type": "application/yaml",
+        }
+      });
+    }
+
+   //Print plain text IP Only if request is to ip.pkgforge.dev without any path suffix
+    return new Response(ip, {
+      headers: {
+        ...CORS_HEADERS,
+        "Content-Type": "text/plain",
+        "x-client-ip": ip
+      }
+    });
+  }
+};
+
+export {
+  src_default as default
+};
+//# sourceMappingURL=index.js.map
